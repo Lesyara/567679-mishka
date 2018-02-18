66df43cab1be6bea71ea2f2ca0c530e0578e8976
Lesyara <lesya.arhipenko@gmail.com>
2018-02-18 03:16:41 +0300

    все состояния элементов, исправлены ошибки в бэм

--- source/js/script.js
+++ source/js/script.js
@@ -3,12 +3,28 @@ var user_menu = document.querySelector(".main-header__user-menu");
 var toogle_button = document.querySelector(".main-header__toggle-menu");
 
 
+window.addEventListener("load", function (evt) {
+
+  evt.preventDefault();
+  if (main_menu.classList.contains("main-header__menu--no-js")) {
+    main_menu.classList.remove("main-header__menu--no-js");
+  }
+
+  if (user_menu.classList.contains("main-header__user-menu--no-js")) {
+    user_menu.classList.remove("main-header__user-menu--no-js");
+  }
+
+  if (toogle_button.classList.contains("main-header__toggle-menu--no-js")) {
+    toogle_button.classList.remove("main-header__toggle-menu--no-js");
+  }
+})
+
 toogle_button.addEventListener("click", function (evt) {
 
   evt.preventDefault();
 
-  if(main_menu.classList.contains("main-header__menu--open") && user_menu.classList.contains("main-header__user-menu--open"))
-  {
+  if (main_menu.classList.contains("main-header__menu--open") &&
+    user_menu.classList.contains("main-header__user-menu--open")) {
     main_menu.classList.remove("main-header__menu--open");
     main_menu.classList.add("main-header__menu--closed");
     user_menu.classList.remove("main-header__user-menu--open");
@@ -16,12 +32,12 @@ toogle_button.addEventListener("click", function (evt) {
     toogle_button.classList.remove("main-header__toggle-menu--close");
     toogle_button.classList.add("main-header__toggle-menu--open");
   }
-  else if (main_menu.classList.contains("main-header__menu--closed") && user_menu.classList.contains("main-header__user-menu--closed"))
-  {
+  else if (main_menu.classList.contains("main-header__menu--closed") &&
+    user_menu.classList.contains("main-header__user-menu--closed")) {
     main_menu.classList.remove("main-header__menu--closed");
     main_menu.classList.add("main-header__menu--open");
     user_menu.classList.remove("main-header__user-menu--closed");
-    main_menu.classList.add("main-header__user-menu--open");
+    user_menu.classList.add("main-header__user-menu--open");
     toogle_button.classList.remove("main-header__toggle-menu--open");
     toogle_button.classList.add("main-header__toggle-menu--close");
   }
